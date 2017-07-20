e24b417341437d25957029bd11ae440bb6340fc4
GLADYSYOVANAMAMANICAMATICONA <roxsy.lj@gmail.com>
2017-07-20 18:01:59 -0500

    asientos-1

---
+++ js.js
@@ -0,0 +1,27 @@
+var celdas = document.getElementsByTagName('td');
+for (var i = 0; i < celdas.length; i++) {
+    celdas[i].addEventListener('click',redirect,false);
+}
+function redirect(event){
+    document.getElementById("mostrar").innerHTML=(event.target.textContent);
+
+}
+function reservarAsintos(){
+	var n=document.getElementById("mostrar").textContent;
+	var nombre=document.getElementById("nombre").value;
+	var apellido=document.getElementById("apellido").value;
+	var dni=document.getElementById("dni").value;
+	
+	
+
+console.log(n)
+}
+
+function Mostrar(){
+	var resultado ="";
+	resultado += "<p>N° de asiento:+n + </p>";
+    resultado += "<p>Nombre:+ nombre+ </p>";
+    resultado += "<p>Apellido:+ apellido + </p>";
+    resultado += "<p>Dni:+ dni + </p>";
+     return resultado;
+}
