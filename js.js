b15ceeb01f4e997808550687890d4eb88394ac2e
GLADYSYOVANAMAMANICAMATICONA <roxsy.lj@gmail.com>
2017-07-21 18:23:40 -0500

    reservaylistado

--- js.js
+++ js.js
@@ -4,28 +4,56 @@ for (var i = 0; i < celdas.length; i++) {
 }
 function redirect(event){
     document.getElementById("mostrar").innerHTML=(event.target.textContent);
+    event.target.style.backgroundColor="red";
 
 }
-function reservarAsintos(){
+
+var arrayDeAsientos = []
+var records = document.getElementById('lista');
+function Pasajero(n,nombre,apellido,dni){
+	this.ndeasiento=n,
+	this.nombrep=nombre,
+	this.apellidop=apellido,
+	this.dnip=dni
+}
+function reservarAsientos(){
 	var n=document.getElementById("mostrar").textContent;
 	console.log(n);
 	var nombre=document.getElementById("nombre").value;
 	var apellido=document.getElementById("apellido").value;
 	var dni=document.getElementById("dni").value;
-	var guardar=document.getElementById("lista");
-	guardar.innerHTML+= "<center><div><h3>ASIENTOS RESERVADOS</h3><p>"+n+"</p><p>Nombre: "+nombre+"</p></div><p>Apellido :"+apellido+"</p><p>Dni :"+dni+"</p></center>";	
-console.log(n)
-}
-function cancelar(){
-   
+	/*var guardar=document.getElementById("lista");
+	guardar.innerHTML+= "<center><div><h3>ASIENTOS RESERVADOS</h3><p>N° de asiento:"+n+"</p><p>Nombre: "+nombre+"</p></div><p>Apellido :"+apellido+"</p><p>Dni :"+dni+"</p></center>";
+console.log(n)*/
+    var reservadeasiento = new Pasajero(n, nombre, apellido, dni);
+    arrayDeAsientos.push(reservadeasiento);
+    console.log(arrayDeAsientos)
 }
 
-function Mostrar(){
-	var resultado ="";
-	resultado += "<p>N° de asiento:+n + </p>";
-    resultado += "<p>Nombre:+ nombre+ </p>";
-    resultado += "<p>Apellido:+ apellido + </p>";
-    resultado += "<p>Dni:+ dni + </p>";
-     return resultado;
+function cancelar(){
+
+}
+function listar(){
+	var listartodo = "";
+		for (var i = 0; i<arrayDeAsientos.length;i++) {
+		listartodo+=arrayDeAsientos[i].ndeasiento+"<br>" 
+				+arrayDeAsientos[i].nombrep+"<br>"
+				+arrayDeAsientos[i].apellidop+"<br>"
+				+arrayDeAsientos[i].dnip+"<br>";
+	}
+	//document.getElementById(listartodo).value;
+document.getElementById("listar").innerHTML=listartodo;
+}
+
+
+
+
+
+function buscar() {
+
+  if (dni==buscardni) {
+
+  }
+
 }
 
