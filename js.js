29421d1458620da98b945a13513b0b4110354df4
GLADYSYOVANAMAMANICAMATICONA <roxsy.lj@gmail.com>
2017-07-20 19:33:52 -0500

    subiendo

--- js.js
+++ js.js
@@ -8,14 +8,12 @@ function redirect(event){
 }
 function reservarAsintos(){
 	var n=document.getElementById("mostrar").textContent;
+	console.log(n);
 	var nombre=document.getElementById("nombre").value;
 	var apellido=document.getElementById("apellido").value;
 	var dni=document.getElementById("dni").value;
 	var guardar=document.getElementById("lista");
-	guardar.innerHTML+= "<center><div><h3>ASIENTOS RESERVADOS</h3><p>Nombre: </p><p>"+nombre+"</p></div><p>Apellido :</p><p>"+apellido+"</p><p>Dni :</p><p>"+dni+"</p></center>";
-
-	
-
+	guardar.innerHTML+= "<center><div><h3>ASIENTOS RESERVADOS</h3><p>"+n+"</p><p>Nombre: "+nombre+"</p></div><p>Apellido :"+apellido+"</p><p>Dni :"+dni+"</p></center>";	
 console.log(n)
 }
 function cancelar(){
