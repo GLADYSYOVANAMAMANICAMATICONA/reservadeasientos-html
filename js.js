5de07f696a8842e9957c4406356bd607a7c47154
GLADYSYOVANAMAMANICAMATICONA <gladysyovanamamanicamaticona@gmail.com>
2017-07-25 00:03:24 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,19 +1,30 @@
+
 var celdas = document.getElementsByTagName('td');
 for (var i = 0; i < celdas.length; i++) {
     celdas[i].addEventListener('click',redirect,false);
 }
 function redirect(event){
+    limpiar();
     document.getElementById("mostrar").innerHTML=(event.target.textContent);
     pintar=event.target;
+    var entero = parseInt(event.target.textContent);
+    for (var i = 0; i < arrayDeAsientos.length; i++) {
+    var almacenar = arrayDeAsientos[i].ndeasiento;
+    if (entero==almacenar) {
+      document.getElementById("nombre").value=arrayDeAsientos[i].nombrep;
+    	document.getElementById("apellido").value=arrayDeAsientos[i].apellidop;
+    	document.getElementById("dni").value=arrayDeAsientos[i].dnip;
 
+    }
+    }
 }
 
-var arrayDeAsientos = []
+var arrayDeAsientos = [];
 //var records = document.getElementById('lista');
 function Pasajero(n,nombre,apellido,dni){
-	this.ndeasiento = n,
-	this.nombrep = nombre,
-	this.apellidop = apellido,
+	this.ndeasiento = n;
+	this.nombrep = nombre;
+	this.apellidop = apellido;
 	this.dnip = dni
 };
 
@@ -33,7 +44,6 @@ limpiar()
 
 }
 
-
 function cancelar(){
 
 }
@@ -50,16 +60,16 @@ console.log(listartodo)
 document.getElementById("lista").innerHTML=listartodo;
 }
 
-
-
-
-
-function buscar() {
-
-  if (dni==buscardni) {
-
+function buscarDni(){
+  var buscarPorDni=parseInt(document.getElementById("dni1").value);
+  for(var i = 0; i<arrayDeAsientos.length; i++){
+  var dniexistente = arrayDeAsientos[i].dnip;
+    if(buscarPorDni== dniexistente){
+      document.getElementById("nombre").value = arrayDeAsientos[i].nombrep;
+      document.getElementById("apellido").value = arrayDeAsientos[i].apellidop;
+      document.getElementById("dni").value = arrayDeAsientos[i].dnip;
+    }
   }
-
 }
 function limpiar(){
   document.getElementById("mostrar").value="";
